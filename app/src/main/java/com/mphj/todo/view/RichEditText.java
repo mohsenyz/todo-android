35641c762919da3986d58e5b6be4eae6219c003b
Mohsen yazdani <alimohammadi340@gmail.com>
2018-12-12 17:59:49 +0330

    Initial commit

---
+++ app/src/main/java/com/mphj/todo/view/RichEditText.java
@@ -0,0 +1,23 @@
+package com.mphj.todo.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.EditText;
+
+public class RichEditText extends EditText {
+    public RichEditText(Context context) {
+        super(context);
+    }
+
+    public RichEditText(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public RichEditText(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public RichEditText(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+}
